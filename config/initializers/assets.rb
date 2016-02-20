0f9be8363b87df2d701f8be96ea6769259e6788e
citlas <citlalli.prado@gmail.com>
2016-02-20 22:20:29 +0100

    added color and price field

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( bike.jpg )
+Rails.application.config.assets.precompile += %w( pbike.jpg )
+Rails.application.config.assets.precompile += %w( pbike2.jpg )
+Rails.application.config.assets.precompile += %w( pbike3.jpg )
+Rails.application.config.assets.precompile += %w( pbike4.jpg )
+Rails.application.config.assets.precompile += %w( pbike40.jpg )
