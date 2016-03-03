aa645339856c907fb817270de85540b6df913edf
citlas <citlalli.prado@gmail.com>
2016-03-03 23:11:14 +0100

    added mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,14 @@ class StaticPagesController < ApplicationController
   	@products = Product.all
   end
 
+def thank_you
+	@name = params[:name]
+	@email = params[:email]
+	@message = params[:message]
+	ActionMailer::Base.mail(:from => @email,
+		:to => "citlalli.prado@gmail.com",
+		:subject => "A new contact form message from #{@name}",
+		:body => @message).deliver_now
+end
+
 end
