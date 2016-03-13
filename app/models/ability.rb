e0415a553ac54fa14695dbe740e6881e5c0a3aa3
citlas <citlalli.prado@gmail.com>
2016-03-13 21:38:18 +0100

    admin destroy

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new #guest user - not logged in
+    if user.admin?
+        can [:manage, :destroy], [:all, Comment]
+    else
     can :manage, User, id: user.id
     end
+end
 end  
     # Define abilities for the passed in user here. For example:
     #
