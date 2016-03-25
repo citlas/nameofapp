6980a3eaa9dfaa2fde6f6b903d0bd334716ce1f6
citlas <citlalli.prado@gmail.com>
2016-03-25 16:43:13 +0100

    still stuck

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,9 @@ class Ability
   def initialize(user)
     user ||= User.new #guest user - not logged in
     if user.admin?
-        can [:manage, :destroy], [:all, Comment]
+        can [:manage, :destroy], [:all]
     else
-    can :manage, User, id: user.id
+    cannot :destroy, Comment
     end
 end
 end  
