7c48b5d832e675e987107572c959bb97953e3677
citlas <citlalli.prado@gmail.com>
2016-05-07 11:27:00 +0200

    fixed prod controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  respond_to :json; :html
+  respond_to :json, :html
 
   # GET /products
   # GET /products.json
